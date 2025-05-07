99a6ab2b27e608da8ea4603be534d040f896c332
Dmitry Berlov <52217850+noir1915@users.noreply.github.com>
2025-05-07 15:34:54 +0400

    Delete user_subscription_service directory

--- user_subscription_service/src/main/java/com/example/user_subscription_service/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_subscription_service.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- user_subscription_service/src/main/java/com/example/user_subscription_service/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.user_subscription_service.repository;
-
-import com.example.user_subscription_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
